d587099b72c576182bb67c9c79b92fa19685b77b
misachi <misachibrian@gmail.com>
2020-08-03 01:12:53 +0300

    Add comments to util function string_to_upper

--- util.h
+++ util.h
@@ -8,6 +8,9 @@
 inline std::string string_to_upper(std::string str) {
     std::string &temp = str;
     std::for_each(std::begin(temp), std::end(temp), [](char& c) {
+        /* from the docs the, argument to toupper should be representable as
+            unsigned char or EOF or else it's UB
+         */
         c = static_cast<char>(std::toupper(static_cast<unsigned char>(c)));
     });
     return temp;
